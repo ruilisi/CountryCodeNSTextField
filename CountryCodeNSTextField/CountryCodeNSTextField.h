88b002d1ccbf890a309029b8c702c1f60448f179
Jiawei Li <c@rallets.com>
2018-06-23 14:34:40 +0800

    Initial Commit

---
+++ CountryCodeNSTextField/CountryCodeNSTextField.h
@@ -0,0 +1,19 @@
+//
+//  CountryCodeNSTextField.h
+//  CountryCodeNSTextField
+//
+//  Created by Jiawei Li on 2018/6/23.
+//  Copyright © 2018 Jiawei Li. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CountryCodeNSTextField.
+FOUNDATION_EXPORT double CountryCodeNSTextFieldVersionNumber;
+
+//! Project version string for CountryCodeNSTextField.
+FOUNDATION_EXPORT const unsigned char CountryCodeNSTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountryCodeNSTextField/PublicHeader.h>
+
+
